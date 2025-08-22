951e0218c3896230353fda764cbbb9b0f914244a
eitanabrishami <abrishami@gmail.com>
2025-08-22 15:28:12 -0700

    MovieCard component updated displays poster, title, year properly and aligned/styled well

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -2,24 +2,36 @@ import {View, Text, Image, TouchableOpacity} from "react-native";
 import React from "react";
 import {Link} from "expo-router";
 import {TOUCHABLE_STATE} from "react-native-gesture-handler/lib/typescript/components/touchables/GenericTouchable";
-// const MovieCard = ({imdbID, Poster, Title, Year}: Movie) => {
-//     return(
-        // asChild means it can be clickable
-        // <Link href={`/Movies/${id}`} asChild>
-        // <TouchableOpacity className={"w-[30%]"}>
-        // <Image
-        //     source={ {
-        //         uri:
-        //
-        //     }}
-        //
-        //
-        //
-        // />
-        //     </TouchableOpacity>
-        // </Link>
+import {icons} from "@/constants/icons";
+const MovieCard = ({imdbID, Poster, Title, Year, imdbRating}: Movie) => {
+     return(
+         console.log(imdbID),
+       // asChild means it can be clickable
+        <Link href={`/Movies/${imdbID}`} asChild>
+        <TouchableOpacity className={"w-[30%]"}>
+        <Image
+            source={ {
+                uri:  Poster,
 
-//     )
-// }
-//
-// export default MovieCard;
+            }}
+            className = "w-full h-52 rounded-lg"
+            resizeMode={"cover"}
+
+
+        />
+            <Text className={"text-sm font-bold color-white mt-2"}>{Title}</Text>
+            {/*<View className={"flex-row items-center justify-start gap-x-1"}>*/}
+            {/*    <Image source={icons.star} className={"size-4"}/>*/}
+            {/*    <Text className = "text-xs text-white font-bold">{Math.round(imdbRating/2)}</Text>*/}
+            {/*</View>*/}
+            <View className={"flex-row items-center justify-between"}>
+                <Text className = "text-xs text-light-300 font-bold">{Year}</Text>
+            </View>
+            </TouchableOpacity>
+        </Link>
+
+
+    )
+}
+
+export default MovieCard;
